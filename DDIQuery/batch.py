e2275597cbb7e0fa618d8bf1e1c5226dff16c81d
FrozenFeather <hlvong7@gpu39.cse.cuhk.edu.hk>
2019-11-29 18:51:18 +0800

    Adopt KFold Cross-Validation

--- DDIQuery/batch.py
+++ DDIQuery/batch.py
@@ -7,180 +7,181 @@ from sklearn.model_selection import KFold
 test_ratio = 0.15
 
 class Repository():
-    def __init__(self, adjcent_matrix, input_ids, output_ids, drug_onehot, drug_feat):
-        self.adjcent_matrix = adjcent_matrix
-        self.drug_onehot = drug_onehot
-        self.onehot_size = drug_onehot.shape[1]
-        #those four attributes only used in blind test
-        self.input_ids = input_ids
-        self.output_ids = output_ids
-        self.drug_feat = drug_feat
-        self.input_feat = drug_feat[input_ids]
-        self.output_feat = drug_feat[output_ids]
+	def __init__(self, adjcent_matrix, input_ids, output_ids, drug_onehot, drug_feat):
+		self.adjcent_matrix = adjcent_matrix
+		self.drug_onehot = drug_onehot
+		self.onehot_size = drug_onehot.shape[1]
+		#those four attributes only used in blind test
+		self.input_ids = input_ids
+		self.output_ids = output_ids
+		self.drug_feat = drug_feat
+		self.input_feat = drug_feat[input_ids]
+		self.output_feat = drug_feat[output_ids]
 
-        self.length = self.adjcent_matrix.shape[0]
+		self.length = self.adjcent_matrix.shape[0]
 
-        self.positions = np.arange(self.length)
-        np.random.shuffle(self.positions)
+		self.positions = np.arange(self.length)
+		np.random.shuffle(self.positions)
 
-        self.offset = 0
-        self.Epoch = True
-
-
-    def miniBatch(self, batch_size):
-        if self.offset + batch_size <= self.length:
-            posi = self.positions[self.offset : self.offset + batch_size]
-        else:
-            posi = self.positions[self.offset : self.length]
-            self.Epoch = False
-            np.random.shuffle(self.positions)
-
-        self.offset += batch_size
-
-        feat_in = self.drug_onehot[posi]
-        feat_out = self.adjcent_matrix[posi]
+		self.offset = 0
+		self.Epoch = True
 
 
-        return torch.FloatTensor(feat_in), \
-                torch.FloatTensor(feat_out)
+	def miniBatch(self, batch_size):
+		if self.offset + batch_size <= self.length:
+			posi = self.positions[self.offset : self.offset + batch_size]
+		else:
+			posi = self.positions[self.offset : self.length]
+			self.Epoch = False
+			np.random.shuffle(self.positions)
 
-    def reset(self):
-        self.offset = 0
-        self.Epoch = True
+		self.offset += batch_size
+
+		feat_in = self.drug_onehot[posi]
+		feat_out = self.adjcent_matrix[posi]
+
+
+		return torch.FloatTensor(feat_in), \
+				torch.FloatTensor(feat_out)
+
+	def reset(self):
+		self.offset = 0
+		self.Epoch = True
 
 class BlindBatch():
-	def __init__(self, adjcent_matrix, k):
+	def __init__(self, adjcent_matrix, k, feature_matrix, radius):
+		self.adjcent_matrix = adjcent_matrix
+		self.k = k
+		self.feature_matrix = feature_matrix
+		self.repos = []
+		self.count = 0
+		
 		size = adjcent_matrix.shape[0]
 		index = np.arange(size)
 		kf = KFold(n_splits=k)
-		for train_index, test_index in kf.split(index):
-			chooseblindtest(adjcent_matrix, train_ids, test_ids)
+		for train_ids, test_ids in kf.split(index):
+			m1, m2, m3, m4 = chooseblindtest(adjcent_matrix, train_ids, test_ids)
+			
+			train_matrix = dilute(m1, radius)
+			
+			train_onehot = np.identity(m1.shape[0])
+			test_onehot = np.identity(adjcent_matrix.shape[0])
+
+			train_repo = Repository(train_matrix, train_ids, train_ids, train_onehot, feature_matrix)
+			test_repo = Repository(adjcent_matrix, train_ids, test_ids, test_onehot, feature_matrix)
+			self.repos.append({'train': train_repo, 'test': test_repo})
 	
 
 def loadBMCData(filename):
-    D = io.loadmat(filename)
-    triple = D["DDI_triple"]
-    binary = D["DDI_binary"]
-    f_offside = D["offsides_feature"]
-    pca_offside = D["pca_offisides"]
-    f_structure = D["structure_feature"]
-    pca_structure = D["pca_structure"]
-    return binary, triple, f_offside, pca_offside, f_structure, pca_structure
+	D = io.loadmat(filename)
+	triple = D["DDI_triple"]
+	binary = D["DDI_binary"]
+	f_offside = D["offsides_feature"]
+	pca_offside = D["pca_offisides"]
+	f_structure = D["structure_feature"]
+	pca_structure = D["pca_structure"]
+	return binary, triple, f_offside, pca_offside, f_structure, pca_structure
 
 def flat(matrix):
-    row, col = np.where(matrix>0)
-    m  = matrix
-    for i in range(len(row)):
-        m[row[i], col[i]] = 1
-    return m
+	row, col = np.where(matrix>0)
+	m  = matrix
+	for i in range(len(row)):
+		m[row[i], col[i]] = 1
+	return m
 
 def dilute(adjcent_matrix, step):
-    m = adjcent_matrix + np.identity(adjcent_matrix.shape[0])
-    temp = adjcent_matrix + np.identity(adjcent_matrix.shape[0])
-    m_list = [m]
-    for i in range(1, step):
-        m  = np.matmul(m, temp)
-        m_list.append(m)
-    
-    temp = m_list[0]
-    for i in range(1, step):
-        temp += m_list[i]
-    return flat(temp) - np.identity(adjcent_matrix.shape[0])
+	m = adjcent_matrix + np.identity(adjcent_matrix.shape[0])
+	temp = adjcent_matrix + np.identity(adjcent_matrix.shape[0])
+	m_list = [m]
+	for i in range(1, step):
+		m  = np.matmul(m, temp)
+		m_list.append(m)
+	
+	temp = m_list[0]
+	for i in range(1, step):
+		temp += m_list[i]
+	return flat(temp) - np.identity(adjcent_matrix.shape[0])
 
 
 def erosion(adjcent_matrix, positions):
-    m  = np.copy(adjcent_matrix)
-    for row, col in positions:
-        m[row, col] = 0
-    return m
+	m  = np.copy(adjcent_matrix)
+	for row, col in positions:
+		m[row, col] = 0
+	return m
 
 def choosenormaltest(adjcent_matrix, ratio):
-    rows, cols = np.where(adjcent_matrix == 1)
-    positions = np.array([x for x in zip(rows, cols)])
-    np.random.shuffle(positions)
-    length = len(positions)
-    test_length = int(length * ratio)
-    test_posi = positions[:test_length]
-    return test_posi
+	rows, cols = np.where(adjcent_matrix == 1)
+	positions = np.array([x for x in zip(rows, cols)])
+	np.random.shuffle(positions)
+	length = len(positions)
+	test_length = int(length * ratio)
+	test_posi = positions[:test_length]
+	return test_posi
 
-def chooseblindtest(adjcent_matrix, ratio):
-    length = adjcent_matrix.shape[0]
-    test_length = int(length * ratio)
-    ids = list(range(length))
-    np.random.shuffle(ids)
-    test_ids = ids[:test_length]
-    train_ids = ids[test_length:]
-    
-    m1 = adjcent_matrix[train_ids, :][:, train_ids]
-    m2 = adjcent_matrix[train_ids, :][:, test_ids]
-    m3 = adjcent_matrix[test_ids, :][:, train_ids]
-    m4 = adjcent_matrix[test_ids, :][:, test_ids]
-    return m1, m2, m3, m4, train_ids, test_ids
+def chooseblindtest(adjcent_matrix, train_ids, test_ids):
+	m1 = adjcent_matrix[train_ids, :][:, train_ids]
+	m2 = adjcent_matrix[train_ids, :][:, test_ids]
+	m3 = adjcent_matrix[test_ids, :][:, train_ids]
+	m4 = adjcent_matrix[test_ids, :][:, test_ids]
+	return m1, m2, m3, m4
 
 def loadnormaldata(radius):
-    adjcent_matrix, _, f1, f2, f3, f4 = loadBMCData("data/DDI.mat")
-    # adjcent_matrix, f3 = loadBMCData("data/DDI.mat")
+	adjcent_matrix, _, f1, f2, f3, f4 = loadBMCData("data/DDI.mat")
+	# adjcent_matrix, f3 = loadBMCData("data/DDI.mat")
 
-    test_posi = choosenormaltest(adjcent_matrix, test_ratio)
+	test_posi = choosenormaltest(adjcent_matrix, test_ratio)
 
-    test_matrix = np.zeros(shape=adjcent_matrix.shape)
-    for row, col in test_posi:
-        test_matrix[row, col] = 1
+	test_matrix = np.zeros(shape=adjcent_matrix.shape)
+	for row, col in test_posi:
+		test_matrix[row, col] = 1
 
-    range_matrix = dilute(adjcent_matrix, radius)
-    train_matrix = erosion(range_matrix, test_posi)
-    
-    onehot = np.identity(adjcent_matrix.shape[0])
-    feat = f3
+	range_matrix = dilute(adjcent_matrix, radius)
+	train_matrix = erosion(range_matrix, test_posi)
+	
+	onehot = np.identity(adjcent_matrix.shape[0])
+	feat = f3
 
-    train_in = np.where(train_matrix.sum(axis=1) > 0)
-    train_out = np.where(train_matrix.sum(axis=0) > 0)
+	train_in = np.where(train_matrix.sum(axis=1) > 0)
+	train_out = np.where(train_matrix.sum(axis=0) > 0)
 
-    test_in = np.where(test_matrix.sum(axis=1) > 0)
-    test_out = np.where(test_matrix.sum(axis=0) > 0)    
+	test_in = np.where(test_matrix.sum(axis=1) > 0)
+	test_out = np.where(test_matrix.sum(axis=0) > 0)	
 
-    print("original number of 1: " + str(np.where(train_matrix == 1)[0].size))
-    print("test number of 1: " + str(np.where(adjcent_matrix == 1)[0].size))
-    
-    return Repository(train_matrix, train_in, train_out, onehot, feat), \
-            Repository(adjcent_matrix, test_in, test_out, onehot, feat)
+	print("original number of 1: " + str(np.where(train_matrix == 1)[0].size))
+	print("test number of 1: " + str(np.where(adjcent_matrix == 1)[0].size))
+	
+	return Repository(train_matrix, train_in, train_out, onehot, feat), \
+			Repository(adjcent_matrix, test_in, test_out, onehot, feat)
 
 def loadwholedata(radius):
-    adjcent_matrix, _, f1, f2, f3, f4 = loadBMCData("data/DDI.mat")
-    
-    test_posi = choosenormaltest(adjcent_matrix, 0.1)
+	adjcent_matrix, _, f1, f2, f3, f4 = loadBMCData("data/DDI.mat")
+	
+	test_posi = choosenormaltest(adjcent_matrix, 0.1)
 
-    test_matrix = np.zeros(shape=adjcent_matrix.shape)
-    for row, col in test_posi:
-        test_matrix[row, col] = 1
+	test_matrix = np.zeros(shape=adjcent_matrix.shape)
+	for row, col in test_posi:
+		test_matrix[row, col] = 1
 
-    train_matrix = dilute(adjcent_matrix, radius)
-    
-    onehot = np.identity(adjcent_matrix.shape[0])
-    feat = f3
+	train_matrix = dilute(adjcent_matrix, radius)
+	
+	onehot = np.identity(adjcent_matrix.shape[0])
+	feat = f3
 
-    train_in = np.where(train_matrix.sum(axis=1) > 0)
-    train_out = np.where(train_matrix.sum(axis=0) > 0)
+	train_in = np.where(train_matrix.sum(axis=1) > 0)
+	train_out = np.where(train_matrix.sum(axis=0) > 0)
 
-    test_in = np.where(test_matrix.sum(axis=1) > 0)
-    test_out = np.where(test_matrix.sum(axis=0) > 0)    
-    
-    return Repository(train_matrix, train_in, train_out, onehot, feat), \
-            Repository(adjcent_matrix, test_in, test_out, onehot, feat)
+	test_in = np.where(test_matrix.sum(axis=1) > 0)
+	test_out = np.where(test_matrix.sum(axis=0) > 0)	
+	
+	return Repository(train_matrix, train_in, train_out, onehot, feat), \
+			Repository(adjcent_matrix, test_in, test_out, onehot, feat)
 
-def loadblinddata(radius):
-    adjcent_matrix, _, f1, f2, f3, f4 = loadBMCData("data/DDI.mat")
-    # adjcent_matrix, f3 = loadBMCData("data/DDI.mat")
-    m1, m2, m3, m4, train_ids, test_ids = chooseblindtest(adjcent_matrix, test_ratio)
-
-    train_matrix = dilute(m1, radius)
-    
-    train_onehot = np.identity(m1.shape[0])
-    test_onehot = np.identity(adjcent_matrix.shape[0])
-
-    return Repository(train_matrix, train_ids, train_ids, train_onehot, np.hstack((f2,f4))), \
-            Repository(adjcent_matrix, train_ids, test_ids, test_onehot, np.hstack((f2,f4)))
+def loadblinddata(radius, no_of_splits):
+	adjcent_matrix, _, f1, f2, f3, f4 = loadBMCData("data/DDI.mat")
+	feature_matrix = np.hstack((f2,f4))
+	batch = BlindBatch(adjcent_matrix, no_of_splits, feature_matrix, radius)
+	return batch
 
 if __name__ == "__main__":
-    train, test = loadblinddata(1)
-    test.miniBatch(5000)
+	train, test = loadblinddata(1)
+	test.miniBatch(5000)
--- DDIQuery/drug2vector.py
+++ DDIQuery/drug2vector.py
@@ -14,9 +14,10 @@ import matplotlib.pyplot as plt
 # parameters
 mode = 'blind'	 # Mode: 'whole', 'normal', 'blind'
 radius = 1
-epoch = 500
+epoch = 75
 embed_size = 60
 weight_decay = 1.0e-4   # L2 Regularization
+learning_rate = 5.4e-3
 criterion = nn.BCELoss()
 
 class Net(nn.Module):
@@ -55,7 +56,7 @@ def enrichment(order, labels):
 	return sum(bins) / bins.shape[0] / count
 
 def updateweight(target, train_feat, test_feat):
-	reg = Ridge(alpha=0.25)
+	reg = Ridge(alpha=0.6)
 	reg.fit(train_feat, target)
 	test_pred = reg.predict(test_feat)
 	# print('lr score:', reg.score(train_feat, target))
@@ -127,138 +128,152 @@ def load_normal_set():
 
 def load_blind_set():
 
-	train_repos, test_repos = loadblinddata(radius)
-	model = Net(train_repos.onehot_size, embed_size)
-
-	optimizer = optim.Adam(model.parameters(), weight_decay=weight_decay)
-
-	test_input, test_output_onehot = test_repos.miniBatch(568)
-
-	for i in range(epoch):
-		while train_repos.Epoch:
-			train_input, train_output_onehot = train_repos.miniBatch(568)
-			optimizer.zero_grad()
-			sigmoid, pred, embed = model(train_input)
-			loss = criterion(sigmoid.flatten(), train_output_onehot.flatten())
-			loss.backward()
-			optimizer.step()
-
-		train_repos.reset()
-
-	test_model = Net(test_repos.onehot_size, embed_size)
-	u,v = model.getWeight()
-	new_u = updateweight(u.transpose(), test_repos.input_feat, test_repos.drug_feat).transpose()
-
-	tr = test_repos.input_ids
-	te = test_repos.output_ids
-
-	# print('size', len(tr)+len(te))
-	# embed_vector = new_u.transpose()
-	embed_vector = np.matmul(test_input.data.numpy(), new_u.transpose())
-	m1 = np.matmul(embed_vector[tr, :], v.transpose())
-	m3 = np.matmul(embed_vector[te, :], v.transpose())
-	m13 = np.matmul(embed_vector, v.transpose())
-	m2 = np.transpose(m3)
-	# m12 = np.zeros((train_repos.onehot_size,test_repos.onehot_size))
-	# m12[:, tr] = m1
-	# m12[:, te] = m2
-	# m12 = np.transpose(m13)
-	# print(embed_vector[test_repos.input_ids].shape, m2.shape)
-	# reg = Ridge(alpha=1e-3)
-	reg = LinearRegression()
-	reg.fit(embed_vector[tr, :], m2)
-	# print(reg.score(embed_vector[tr, :], m2))
-	# print(v.shape, reg.coef_.shape)
-	new_v = np.zeros((test_repos.onehot_size, embed_size))
-	# new_v = reg.coef_
-	# m2 = np.matmul(embed_vector[tr,:], new_v[te,:].transpose())
-	# m12 = np.matmul(embed_vector[tr,:], new_v.transpose())
-	new_v[tr, :] = v
-	new_v[te, :] = reg.coef_
-
-	# m1_l = test_output_onehot.data.numpy()[tr,:][:,tr]
-	# auc1 = metrics.roc_auc_score(m1_l.flatten(), m1.flatten())
-	# aupr1 = metrics.average_precision_score(m1_l.flatten(), m1.flatten())
-	# print('m1_t', auc1, aupr1)
-	# m2_l = test_output_onehot.data.numpy()[tr,:][:,te]
-	# auc2 = metrics.roc_auc_score(m2_l.flatten(), m2.flatten())
-	# aupr2 = metrics.average_precision_score(m2_l.flatten(), m2.flatten())
-	# print('m2_t', auc2, aupr2)
-	# m3_l = test_output_onehot.data.numpy()[te,:][:,tr]
-	# auc3 = metrics.roc_auc_score(m3_l.flatten(), m3.flatten())
-	# aupr3 = metrics.average_precision_score(m3_l.flatten(), m3.flatten())
-	# print('m3_t', auc3, aupr3)
-	# m13_l = test_output_onehot.data.numpy()[:,tr]
-	# auc13 = metrics.roc_auc_score(m13_l.flatten(), m13.flatten())
-	# aupr13 = metrics.average_precision_score(m13_l.flatten(), m13.flatten())
-	# print('m13_t', auc13, aupr13)
-	# m12_l = test_output_onehot.data.numpy()[tr,:]
-	# auc12 = metrics.roc_auc_score(m12_l.flatten(), m12.flatten())
-	# aupr12 = metrics.average_precision_score(m12_l.flatten(), m12.flatten())
-	# print('m12_t', auc12, aupr12)
-
-	#new_v = np.linalg.pinv(updateweight(np.linalg.pinv(v).transpose(), test_repos.input_feat, test_repos.drug_feat)).transpose()
-	test_model.setWeight(new_u, new_v)
-
-	# sigmoid, pred, embed = test_model(test_input)
-	sigmoid, pred, embed = test_model(torch.FloatTensor(np.identity(test_repos.onehot_size)))
-	out = sigmoid.data.numpy()
-	# labels = test_output_onehot.data.numpy()
-	labels = test_repos.adjcent_matrix
-
-	# tr = np.sort(test_repos.input_ids)
-	# te = np.sort(test_repos.output_ids)
-
-	m1_p = out[tr, :][:, tr].flatten()
-	m1_l = labels[tr, :][:, tr].flatten()
-
-	auc1 = metrics.roc_auc_score(m1_l, m1_p)
-	aupr1 = metrics.average_precision_score(m1_l, m1_p)
-	print('tr x tr', auc1, aupr1)
-
-	# m2_p = out[tr, :][:, te].flatten()
-	# m2_l = labels[tr, :][:, te].flatten()
-
-	# auc2 = metrics.roc_auc_score(m2_l, m2_p)
-	# aupr2 = metrics.average_precision_score(m2_l, m2_p)
-	# print('tr x te', auc2, aupr2)
-
-	m3_p = out[te, :][:, tr].flatten()
-	m3_l = labels[te, :][:, tr].flatten()
-
-	auc3 = metrics.roc_auc_score(m3_l, m3_p)
-	aupr3 = metrics.average_precision_score(m3_l, m3_p)
-	print('te x tr', auc3, aupr3)
-
-	# m4_p = out[te, :][:, te].flatten()
-	# m4_l = labels[te, :][:, te].flatten()
-
-	# auc4 = metrics.roc_auc_score(m4_l, m4_p)
-	# aupr4 = metrics.average_precision_score(m4_l, m4_p)
-	# print('te x te', auc4, aupr4)
+	batch = loadblinddata(radius, 10)
+	auc1s = np.zeros(10)
+	aupr1s = np.zeros(10)
+	auc3s = np.zeros(10)
+	aupr3s = np.zeros(10)
+	auc13s = np.zeros(10)
+	aupr13s = np.zeros(10)
 	
-	m13_p = out[:, tr].flatten()
-	m13_l = labels[:, tr].flatten()
+	for index, repos in enumerate(batch.repos):
+		train_repos, test_repos = repos["train"], repos["test"]
+		model = Net(train_repos.onehot_size, embed_size)
 
-	auc13 = metrics.roc_auc_score(m13_l, m13_p)
-	aupr13 = metrics.average_precision_score(m13_l, m13_p)
-	print('All x tr', auc13, aupr13)
+		optimizer = optim.Adam(model.parameters(), weight_decay=weight_decay, lr=learning_rate)
 
-	# auc = metrics.roc_auc_score(labels.flatten(), out.flatten())
-	# aupr = metrics.average_precision_score(labels.flatten(), out.flatten())
-	# print('Overall', auc, aupr)
-	# print(np.where(labels[tr, :][:, te]!=labels[te, :][:, tr].transpose()))
+		test_input, test_output_onehot = test_repos.miniBatch(568)
 
-	export_roc_curve(m13_l, m13_p, auc13)
-	export_pr_curve(m13_l, m13_p, aupr13)
-		# export_roc_curve(labels, out, auc)
-		# export_pr_curve(labels, out, aupr)
-	
-	# whole_input = torch.FloatTensor(np.identity(test_repos.onehot_size))
-	# sigmoid, pred, embed = test_model(whole_input)
-	# if i%10 == 9:
-		# np.save(str(i) + ".npy", embed.data.numpy())
-		# print("saved!")
+		for i in range(epoch):
+			while train_repos.Epoch:
+				train_input, train_output_onehot = train_repos.miniBatch(568)
+				optimizer.zero_grad()
+				sigmoid, pred, embed = model(train_input)
+				loss = criterion(sigmoid.flatten(), train_output_onehot.flatten())
+				loss.backward()
+				optimizer.step()
+
+			train_repos.reset()
+
+		test_model = Net(test_repos.onehot_size, embed_size)
+		u,v = model.getWeight()
+		new_u = updateweight(u.transpose(), test_repos.input_feat, test_repos.drug_feat).transpose()
+
+		tr = test_repos.input_ids
+		te = test_repos.output_ids
+
+		# print('size', len(tr)+len(te))
+		# embed_vector = new_u.transpose()
+		embed_vector = np.matmul(test_input.data.numpy(), new_u.transpose())
+		m1 = np.matmul(embed_vector[tr, :], v.transpose())
+		m3 = np.matmul(embed_vector[te, :], v.transpose())
+		m13 = np.matmul(embed_vector, v.transpose())
+		m2 = np.transpose(m3)
+		# m12 = np.zeros((train_repos.onehot_size,test_repos.onehot_size))
+		# m12[:, tr] = m1
+		# m12[:, te] = m2
+		# m12 = np.transpose(m13)
+		# print(embed_vector[test_repos.input_ids].shape, m2.shape)
+		# reg = Ridge(alpha=1e-3)
+		reg = LinearRegression()
+		reg.fit(embed_vector[tr, :], m2)
+		# print(reg.score(embed_vector[tr, :], m2))
+		# print(v.shape, reg.coef_.shape)
+		new_v = np.zeros((test_repos.onehot_size, embed_size))
+		# new_v = reg.coef_
+		# m2 = np.matmul(embed_vector[tr,:], new_v[te,:].transpose())
+		# m12 = np.matmul(embed_vector[tr,:], new_v.transpose())
+		new_v[tr, :] = v
+		new_v[te, :] = reg.coef_
+
+		# m1_l = test_output_onehot.data.numpy()[tr,:][:,tr]
+		# auc1 = metrics.roc_auc_score(m1_l.flatten(), m1.flatten())
+		# aupr1 = metrics.average_precision_score(m1_l.flatten(), m1.flatten())
+		# print('m1_t', auc1, aupr1)
+		# m2_l = test_output_onehot.data.numpy()[tr,:][:,te]
+		# auc2 = metrics.roc_auc_score(m2_l.flatten(), m2.flatten())
+		# aupr2 = metrics.average_precision_score(m2_l.flatten(), m2.flatten())
+		# print('m2_t', auc2, aupr2)
+		# m3_l = test_output_onehot.data.numpy()[te,:][:,tr]
+		# auc3 = metrics.roc_auc_score(m3_l.flatten(), m3.flatten())
+		# aupr3 = metrics.average_precision_score(m3_l.flatten(), m3.flatten())
+		# print('m3_t', auc3, aupr3)
+		# m13_l = test_output_onehot.data.numpy()[:,tr]
+		# auc13 = metrics.roc_auc_score(m13_l.flatten(), m13.flatten())
+		# aupr13 = metrics.average_precision_score(m13_l.flatten(), m13.flatten())
+		# print('m13_t', auc13, aupr13)
+		# m12_l = test_output_onehot.data.numpy()[tr,:]
+		# auc12 = metrics.roc_auc_score(m12_l.flatten(), m12.flatten())
+		# aupr12 = metrics.average_precision_score(m12_l.flatten(), m12.flatten())
+		# print('m12_t', auc12, aupr12)
+
+		#new_v = np.linalg.pinv(updateweight(np.linalg.pinv(v).transpose(), test_repos.input_feat, test_repos.drug_feat)).transpose()
+		test_model.setWeight(new_u, new_v)
+
+		# sigmoid, pred, embed = test_model(test_input)
+		sigmoid, pred, embed = test_model(torch.FloatTensor(np.identity(test_repos.onehot_size)))
+		out = sigmoid.data.numpy()
+		# labels = test_output_onehot.data.numpy()
+		labels = test_repos.adjcent_matrix
+
+		# tr = np.sort(test_repos.input_ids)
+		# te = np.sort(test_repos.output_ids)
+		
+		print('batch', index)
+		m1_p = out[tr, :][:, tr].flatten()
+		m1_l = labels[tr, :][:, tr].flatten()
+
+		auc1s[index] = metrics.roc_auc_score(m1_l, m1_p)
+		aupr1s[index] = metrics.average_precision_score(m1_l, m1_p)
+		print('tr x tr', auc1s[index], aupr1s[index])
+
+		# m2_p = out[tr, :][:, te].flatten()
+		# m2_l = labels[tr, :][:, te].flatten()
+
+		# auc2 = metrics.roc_auc_score(m2_l, m2_p)
+		# aupr2 = metrics.average_precision_score(m2_l, m2_p)
+		# print('tr x te', auc2, aupr2)
+
+		m3_p = out[te, :][:, tr].flatten()
+		m3_l = labels[te, :][:, tr].flatten()
+
+		auc3s[index] = metrics.roc_auc_score(m3_l, m3_p)
+		aupr3s[index] = metrics.average_precision_score(m3_l, m3_p)
+		print('te x tr', auc3s[index], aupr3s[index])
+
+		# m4_p = out[te, :][:, te].flatten()
+		# m4_l = labels[te, :][:, te].flatten()
+
+		# auc4 = metrics.roc_auc_score(m4_l, m4_p)
+		# aupr4 = metrics.average_precision_score(m4_l, m4_p)
+		# print('te x te', auc4, aupr4)
+		
+		m13_p = out[:, tr].flatten()
+		m13_l = labels[:, tr].flatten()
+
+		auc13s[index] = metrics.roc_auc_score(m13_l, m13_p)
+		aupr13s[index] = metrics.average_precision_score(m13_l, m13_p)
+		print('All x tr', auc13s[index], aupr13s[index])
+
+		# auc = metrics.roc_auc_score(labels.flatten(), out.flatten())
+		# aupr = metrics.average_precision_score(labels.flatten(), out.flatten())
+		# print('Overall', auc, aupr)
+		# print(np.where(labels[tr, :][:, te]!=labels[te, :][:, tr].transpose()))
+
+		# export_roc_curve(m13_l, m13_p, auc13)
+		# export_pr_curve(m13_l, m13_p, aupr13)
+			# export_roc_curve(labels, out, auc)
+			# export_pr_curve(labels, out, aupr)
+		
+		# whole_input = torch.FloatTensor(np.identity(test_repos.onehot_size))
+		# sigmoid, pred, embed = test_model(whole_input)
+		# if i%10 == 9:
+			# np.save(str(i) + ".npy", embed.data.numpy())
+			# print("saved!")
+	print('Average')
+	print('tr x tr', np.mean(auc1s), np.mean(aupr1s))
+	print('te x tr', np.mean(auc3s), np.mean(aupr3s))
+	print('all x tr', np.mean(auc13s), np.mean(aupr13s))
 
 if __name__ == "__main__":
 	np.random.seed()
